d2bdb10bfaf64635d0c61133b442cc6caa0199df
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-05 10:06:31 +0100

    Added NotFoundHandler to handle all non resolvable routes

--- main.go
+++ main.go
@@ -12,6 +12,13 @@ func main() {
 	port := "8080"
 	router := mux.NewRouter()
 
+	// GET "*" handles all unknown routes
+	router.NotFoundHandler = http.HandlerFunc(notFoundRoute)
+
 	log.Println("Server listening on port: ", port)
 	log.Fatalln(http.ListenAndServe(fmt.Sprintf("localhost:%s", port), router))
+}
+
+func notFoundRoute(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "404 Not Found", http.StatusNotFound)
 }
