361b059594685354d6fc98d211ee3ede0f1ee73e
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-05 11:08:09 +0100

    Added CreateNewProduct method for creating new products

--- controllers/products.go
+++ controllers/products.go
@@ -1,27 +1,64 @@
 package controllers
 
 import (
+	"encoding/json"
+	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/abdillahzakkie/silkroad/database"
+	"github.com/abdillahzakkie/silkroad/helpers"
+	"github.com/gorilla/mux"
 )
 
 type Model struct {
-	ID        	uint 			`gorm:"primaryKey" json:"id"`
-	CreatedAt 	time.Time 	`json:"created_at"`
-	UpdatedAt 	time.Time 	`json:"updated_at"`
-	DeletedAt 	*time.Time 	`gorm:"index;<-:update" json:"-"`
+	ID        	uint 		`gorm:"primaryKey" json:"id" schema:"-"`
+	CreatedAt 	time.Time 	`json:"created_at" schema:"-"`
+	UpdatedAt 	time.Time 	`json:"updated_at" schema:"-"`
+	DeletedAt 	*time.Time 	`gorm:"index;<-:update" json:"-" schema:"-"`
 }
 
 type Product struct {
 	Model
-	Name 		string 		`gorm:"<-;not null" json:"name"`
-	Description string 		`gorm:"<-;not null" json:"description"`
-	Price 		float64 	`gorm:"<-;not null" json:"price"`
-	Quantity 	int 		`gorm:"<-;not null" json:"quantity"`
+	SellerId 	uint 		`json:"seller_id" schema:"-"`
+	Name 		string 		`gorm:"<-;not null" json:"name"  schema:"name,required"`
+	Description string 		`gorm:"<-;not null" json:"description" schema:"description,required"`
+	Category 	string 		`gorm:"<-;not null" json:"category" schema:"category,required"`
+	Price 		float64 	`gorm:"<-;not null" json:"price" schema:"price,required"`
+	Quantity 	int 		`gorm:"<-;not null" json:"quantity" schema:"quantity,required"`
 }
 
 func init() {
 	// migrate database
 	database.DB.AutoMigrate(&Product{})
+}
+
+// POST "/products/{seller_id}/new"
+// CreateNewProduct creates new product
+func (p Product) CreateNewProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	sellerId, err  := strconv.Atoi(vars["seller_id"]); if err != nil {
+		helpers.RespondWithError(w, http.StatusBadRequest, "invalid seller id received")
+		return
+	}
+
+	product := Product{
+		SellerId: uint(sellerId),
+	}	
+
+	err = helpers.ParseForm(r, &product); if err != nil {
+		helpers.RespondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	result := database.DB.Create(&product) 
+
+	if result.Error != nil {
+		helpers.RespondWithError(w, http.StatusBadRequest, result.Error.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(product)
 }
