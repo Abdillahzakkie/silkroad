59c2fb2cceac788a50ac1f012d3f11e458cf574d
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-05 22:37:12 +0100

    included status code in the 'RespondWithError' error response

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -28,6 +28,7 @@ func RespondWithError(w http.ResponseWriter, code int, message string) {
 	w.WriteHeader(code)
 	err := json.NewEncoder(w).Encode(map[string]string{
 		"error": message,
+		"code": fmt.Sprintf("%d", code),
 	})
 	
 	if err != nil {
