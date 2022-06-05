ea02c8a1ff2aaa283c7fa7c2974bf7567b9150d5
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-05 22:07:09 +0100

    made some changes to CreateNewProduct method to track if the category_id existed

--- controllers/products.go
+++ controllers/products.go
@@ -2,30 +2,32 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/abdillahzakkie/silkroad/database"
 	"github.com/abdillahzakkie/silkroad/helpers"
+
 	"github.com/gorilla/mux"
 )
 
 type Model struct {
-	ID        	uint 		`gorm:"primaryKey" json:"id" schema:"-"`
-	CreatedAt 	time.Time 	`json:"created_at" schema:"-"`
-	UpdatedAt 	time.Time 	`json:"updated_at" schema:"-"`
-	DeletedAt 	*time.Time 	`gorm:"index;<-:update" json:"-" schema:"-"`
+	ID        uint       `gorm:"primaryKey" json:"id" schema:"-"`
+	CreatedAt time.Time  `json:"created_at" schema:"-"`
+	UpdatedAt time.Time  `json:"updated_at" schema:"-"`
+	DeletedAt *time.Time `gorm:"index;<-:update" json:"-" schema:"-"`
 }
 
 type Product struct {
 	Model
-	SellerId 	uint 		`json:"seller_id" schema:"-"`
-	Name 		string 		`gorm:"<-;not null" json:"name"  schema:"name,required"`
-	Description string 		`gorm:"<-;not null" json:"description" schema:"description,required"`
-	Category 	string 		`gorm:"<-;not null" json:"category" schema:"category,required"`
-	Price 		float64 	`gorm:"<-;not null" json:"price" schema:"price,required"`
-	Quantity 	int 		`gorm:"<-;not null" json:"quantity" schema:"quantity,required"`
+	SellerId 	uint 				`json:"seller_id" schema:"-"`
+	Name 		string 				`gorm:"<-;not null" json:"name" schema:"name,required"`
+	Description string 				`gorm:"<-;not null" json:"description" schema:"description,required"`
+	Category 						// `gorm:"embedded" json:"category" schema:"-"`
+	Price 		float64 			`gorm:"<-;not null" json:"price" schema:"price,required"`
+	Quantity 	int 				`gorm:"<-;not null" json:"quantity" schema:"quantity,required"`
 }
 
 func init() {
@@ -44,16 +46,29 @@ func (p Product) CreateNewProduct(w http.ResponseWriter, r *http.Request) {
 
 	product := Product{
 		SellerId: uint(sellerId),
-	}	
+	}
 
 	err = helpers.ParseForm(r, &product); if err != nil {
 		helpers.RespondWithError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	result := database.DB.Create(&product) 
+	// checks if category ID exists
+	var category Category
+	result := database.DB.Find(&category, product.Category.CategoryId); if result.Error != nil {
+		helpers.RespondWithError(
+			w, 
+			http.StatusBadRequest, 
+			fmt.Sprintf("category id %v does not exist", product.Category.CategoryId),
+		)
+		return
+	}
+	if category.CategoryId == 0 {
+		helpers.RespondWithError(w, http.StatusBadRequest, "category id does not exist")
+		return
+	}
 
-	if result.Error != nil {
+	result = database.DB.Create(&product); if result.Error != nil {
 		helpers.RespondWithError(w, http.StatusBadRequest, result.Error.Error())
 		return
 	}
