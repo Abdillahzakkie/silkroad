c8fe312b9e5c4e571f471ff4b4ec85bf330d6974
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-05 10:10:19 +0100

    Added gorm and gorm-postgres drivers for initializing postgres db connection

--- main.go
+++ main.go
@@ -5,12 +5,24 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/abdillahzakkie/silkroad/database"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	port := "8080"
 	router := mux.NewRouter()
+	sqlDB, err := database.DB.DB(); if err != nil {
+		log.Fatalln(err)
+	}
+	defer sqlDB.Close()
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(5)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(10)
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	// sqlDB.SetConnMaxLifetime(time.Second)
 
 	// GET "*" handles all unknown routes
 	router.NotFoundHandler = http.HandlerFunc(notFoundRoute)
