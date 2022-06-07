075532ab0eca7ca66ec72d5c72ea118b4128318c
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-07 23:13:30 +0100

    Added CreateNewUser method to create new account

--- models/users.go
+++ models/users.go
@@ -1,10 +1,30 @@
 package models
 
+import (
+	"github.com/abdillahzakkie/silkroad/database"
+	"github.com/abdillahzakkie/silkroad/helpers"
+)
+
 type User struct {
 	Model
-	ID       int   `gorm:"primaryKey" json:"id" schema:"-"`
+	ID       int    `gorm:"primaryKey" json:"id" schema:"-"`
 	Wallet   string `gorm:"<-;not null;uniqueIndex" json:"wallet" schema:"wallet,required"`
 	Username string `gorm:"<-;not null;uniqueIndex" json:"username" schema:"username,required"`
 	Email    string `gorm:"<-;not null;uniqueIndex" json:"-" schema:"email,required"`
 	Password string `gorm:"<-;not null" json:"password" schema:"password,required"`
+}
+
+func (u *User) CreateNewUser() error {
+	// hash user's password
+	password, err := helpers.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+	u.Password = string(password)
+
+	result := database.DB.Create(&u)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
 }
