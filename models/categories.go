8f0711fb4e1e6d229988c7657d22162dcf04b985
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-08 15:06:41 +0100

    added CreateNewCategory method

---
+++ models/categories.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"github.com/abdillahzakkie/silkroad/database"
+)
+
+type Category struct {
+	Model
+	CategoryID int   `gorm:"primaryKey" json:"category_id" schema:"category_id"`
+	Name       string `gorm:"<-;not null;uniqueIndex" json:"name" schema:"name"`
+}
+
+// POST "/categories/new"
+// CreateNewCategory creates new category
+func (c *Category) CreateNewCategory() (err error) {
+	err = database.DB.Create(&c).Error; if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c Category) GetAllCategories() (categories []Category, err error) {
+	err = database.DB.Find(&categories).Error; if err != nil {
+		return nil, err
+	}
+	return categories, nil
+}
+
+func (c *Category) GetCategoryById() (err error) {
+	err = database.DB.First(&c, c.CategoryID).Error; if err != nil {
+		return err
+	}
+	return nil
+}
