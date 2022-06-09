2b18a59f80167069f4ade683ae266f5b1d55b68a
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-09 12:55:56 +0100

    removed redundant comments

--- main.go
+++ main.go
@@ -76,7 +76,7 @@ func main() {
 	// GET "/products?seller_id=<seller_id>"
 	// Create new category
 	router.HandleFunc("/products", controllers.GetProductsBySellerId).Queries("seller_id", "{seller_id}").Methods(http.MethodGet)
-	
+
 	startServer(router)
 }
 
@@ -112,54 +112,6 @@ func startServer(r *mux.Router) {
     log.Println("shutting down")
     os.Exit(0)
 }
-
-// func main() {
-// 	var user controllers.User
-// 	var product controllers.Product
-// 	var category controllers.Category
-
-// 	port := "8080"
-// 	router := mux.NewRouter()
-// 	sqlDB, err := database.DB.DB(); if err != nil {
-// 		log.Fatalln(err)
-// 	}
-// 	// defer closing DB connection
-// 	defer sqlDB.Close()
-	
-// 	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-// 	sqlDB.SetMaxIdleConns(5)
-
-// 	// SetMaxOpenConns sets the maximum number of open connections to the database.
-// 	sqlDB.SetMaxOpenConns(10)
-// 	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
-// 	// sqlDB.SetConnMaxLifetime(time.Second)
-
-// 	// GET "*" handles all unknown routes
-// 	router.NotFoundHandler = http.HandlerFunc(notFoundRoute)
-
-// 	// POST "/users/signup"
-// 	// Creates new user
-// 	router.HandleFunc("/users/signup", user.CreateNewUser).Methods(http.MethodPost)
-	
-// 	// POST "/products/{seller_id}/new"
-// 	// Creates new product
-// 	router.HandleFunc("/products/{seller_id}/new", product.CreateNewProduct).Methods(http.MethodPost)
-
-// 	// POST "/categories/new"
-// 	// Creates new categories
-// 	router.HandleFunc("/categories/new", category.CreateNewCategory).Methods(http.MethodPost)
-
-// 	// GET "/categories/"
-// 	// Get all categories
-// 	router.HandleFunc("/categories", category.GetAllCategories).Methods(http.MethodGet)
-
-// 	// GET "/categories/{category_id}"
-// 	// Get category by ID
-// 	router.HandleFunc("/categories/{category_id}", category.GetCategoryById).Methods(http.MethodGet)
-
-// 	log.Println("Server listening on port: ", port)
-// 	log.Fatalln(http.ListenAndServe(fmt.Sprintf("localhost:%s", port), router))
-// }
 
 func notFoundRoute(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "404 Not Found", http.StatusNotFound)
