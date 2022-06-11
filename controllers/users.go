79dd67ce9e6361a8fd5d405d535d09c82888646a
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-11 22:11:58 +0100

    made some changes

--- controllers/users.go
+++ controllers/users.go
@@ -14,32 +14,42 @@ import (
 // POST "/users/new"
 // CreateNewUser creates new user
 func CreateNewUser(w http.ResponseWriter, r *http.Request) {
+	var userService models.UserService
 	var user models.User
+	
 	// parse url-encoded form data
 	err := helpers.ParseForm(r, &user); if err != nil {
-		helpers.RespondWithError(w, http.StatusBadRequest, fmt.Sprintf("%v", err))
+		helpers.RespondWithError(w, http.StatusBadRequest, helpers.ErrorString(err))
 		return 
 	}
 
 	// save user to database
-	err = user.CreateNewUser(); if err != nil {
-		helpers.RespondWithError(w, http.StatusBadRequest, "user has already existed")
-		return
+	response, err := userService.CreateNewUser(&user)
+	switch err {
+		case nil:
+			break
+		case models.ErrorUserAlreadyExists:
+			helpers.RespondWithError(w, http.StatusBadRequest, helpers.ErrorString(err))
+			return
+		default:
+			helpers.RespondWithError(w, http.StatusInternalServerError, helpers.ErrorString(err))
+			return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(user)
+	json.NewEncoder(w).Encode(response)
 }
 
 // GET "/users/all"
 // GetAllUsers queries and returns all users
 func GetAllUsers(w http.ResponseWriter, r *http.Request) {
-	var user models.User
-	users, err := user.GetAllUsers(); if err != nil {
-		helpers.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("%v", err))
+	var userService models.UserService
+	users, err := userService.GetAllUsers(); if err != nil {
+		helpers.RespondWithError(w, http.StatusInternalServerError, helpers.ErrorString(err))
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(users)
@@ -48,18 +58,24 @@ func GetAllUsers(w http.ResponseWriter, r *http.Request) {
 // GET "/users?id=<id>"
 // GetUserById gets user by ID
 func GetUserById(w http.ResponseWriter, r *http.Request) {
-	var user models.User
-	var err error
+	var userService models.UserService
 
 	vars := mux.Vars(r)
-	user.ID, err = strconv.Atoi(vars["id"]); if err != nil {
+	id, err := (strconv.Atoi(vars["id"])); if err != nil {
 		helpers.RespondWithError(w, http.StatusBadRequest, "invalid user ID")
 		return
 	}
 
-	err = user.GetUser(); if err != nil {
-		helpers.RespondWithError(w, http.StatusNotFound, "user does not exist")
-		return
+	user, err := userService.GetUserById(uint(id))
+	switch err {
+		case nil:
+			break
+		case models.ErrorUserNotFound:
+			helpers.RespondWithError(w, http.StatusNotFound, helpers.ErrorString(err))
+			return
+		default:
+			helpers.RespondWithError(w, http.StatusInternalServerError, helpers.ErrorString(err))
+			return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -70,23 +86,35 @@ func GetUserById(w http.ResponseWriter, r *http.Request) {
 // DELETE "/users/:id"
 // DeleteUser deletes user by ID
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	var user models.User
-	var err error
+	var userService models.UserService
 	vars := mux.Vars(r)
 
-	user.ID, err = strconv.Atoi(vars["user_id"]); if err != nil {
+	id, err := strconv.Atoi(vars["user_id"]); if err != nil {
 		helpers.RespondWithError(w, http.StatusNotFound, "invalid user id")
 		return
 	}
 
 	// checks if user exists
-	err = user.GetUser(); if err != nil {
-		helpers.RespondWithError(w, http.StatusNotFound, "user does not exist")
+	user := models.User{
+		ID: uint(id),
+	}
+
+	status, err := userService.IsExistingUser(user)
+	switch err {
+		case nil:
+			break
+		default:
+			helpers.RespondWithError(w, http.StatusInternalServerError, helpers.ErrorString(err))
+			return
+	}
+
+	if !status {
+		helpers.RespondWithError(w, http.StatusNotFound, helpers.ErrorString(models.ErrorUserNotFound))
 		return
 	}
 
-	err = user.DeleteUser(); if err != nil {
-		helpers.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("%v", err))
+	err = userService.DeleteUser(user.ID); if err != nil {
+		helpers.RespondWithError(w, http.StatusNotFound, fmt.Sprintf("%v", err))
 		return
 	}
 
