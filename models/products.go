ff0d08b7e600a55376b730a724917490198a656b
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-11 23:42:18 +0100

    Revert "made some changes"
    This reverts commit 4d7ecc78efa28dc714954310b1da05ede572a2a7.

--- models/products.go
+++ models/products.go
@@ -1,75 +1,36 @@
 package models
 
 import (
-	"errors"
-	"fmt"
-
 	"github.com/abdillahzakkie/silkroad/database"
-	"github.com/jackc/pgconn"
-	"gorm.io/gorm"
-)
-
-var (
-	ErrorProductAlreadyExists = errors.New("product already exists")
-	ErrorProductNotFound = errors.New("product not found")
 )
 
 type Product struct {
 	Model
-	ID 				uint 		`gorm:"primaryKey;column:product_id" json:"id" schema:"-"`
-	UserID			uint			`json:"user_id" schema:"seller_id,required"`	
-	CategoryID      uint     	`json:"category_id" schema:"category_id,required"`
+	ID 				int 		`gorm:"primaryKey;column:product_id" json:"id" schema:"-"`
+	UserID		int			`json:"user_id" schema:"seller_id,required"`	
+	CategoryID      int     	`json:"category_id" schema:"category_id,required"`
 	ProductName 	string  	`gorm:"not null" json:"product_name" schema:"name,required"`
 	Description 	string  	`gorm:"not null" json:"description" schema:"description,required"`
 	Price       	float64 	`gorm:"not null" json:"price" schema:"price,required"`
 	Quantity    	int     	`gorm:"not null" json:"quantity" schema:"quantity,required"`
 }
 
-type ProductService struct {
-	db *gorm.DB
+func (p *Product) CreateNewProduct() error {
+	return database.DB.Create(&p).Error
 }
 
-// create new ProductService
-func NewProductService() *ProductService {
-	us := ProductService{
-		db: database.DB,
-	}
-	return &us
-}
-
-// Close method close the database connection
-func (us *ProductService) Close() error {
-	sql, err := us.db.DB(); if err != nil {
-		return err
-	}
-	sql.Close()
-	return nil
-}
-
-func (ps *ProductService) CreateNewProduct(product Product) (Product, error) {
-	err := database.DB.Create(&product).Error
-	switch err.(type) {
-		case *pgconn.PgError:
-			return product, ErrorProductAlreadyExists
-	}
-	return product, err
-}
-
-func (ps *ProductService) GetAllProducts() (products []Product, err error) {
+func (p *Product) GetAllProducts() (products []Product, err error) {
 	err = database.DB.Find(&products).Error; if err != nil {
 		return nil, err
 	}
 	return products, nil
 }
 
-func (ps ProductService) GetProduct(product Product) (Product, error) {
-	err := database.DB.Where(product).First(&product).Error
-	fmt.Println(err)
-	switch err {
-		case gorm.ErrRecordNotFound:
-			return product, ErrorProductNotFound
+func (p Product) GetProduct() (product Product, err error) {
+	err = database.DB.Where(p).First(&product).Error; if err != nil {
+		return product, err
 	}
-	return product, err
+	return product, nil
 }
 
 func (p *Product) GetProductById() error {
@@ -81,14 +42,4 @@ func (p *Product) GetProductsBySellerId() (products []Product, err error) {
 		return nil, err
 	}
 	return products, nil
-}
-
-func (ps ProductService) IsExistingProduct(product Product) error {
-	err := database.DB.Where(product).First(&product).Error; if err != nil {
-		switch err {
-			case gorm.ErrRecordNotFound:
-				return ErrorUserNotFound
-		}
-	}
-	return err
 }
